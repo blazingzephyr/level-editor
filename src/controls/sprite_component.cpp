e3a5aa842a612ec669fad24a7ec50d6f37a42714
TenaciousKirikot <blazerzeif@gmail.com>
2022-12-29 22:16:48 +0700

    Rewrite SpriteComponent
    Completely remade SpriteComponent - removed unnecessary dynamic memory allocations, replaced raw pointers with smart pointers, simplified the entire code and added comments.

--- src/controls/sprite_component.cpp
+++ src/controls/sprite_component.cpp
@@ -1,51 +1,73 @@
+////////////////////////////////////////////////////////////
+//
+// MIT License
+//
+// Copyright (c) 2022 ZaBlazzingZeif
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
 #include "sprite_component.hpp"
 
-const sf::IntRect& Empty = sf::IntRect();
 
-SpriteComponent::SpriteComponent(const sf::Vector2f& position, const sf::Texture* texture_pointer, const sf::IntRect& sprite) :
-	
-	SpriteComponent(position, texture_pointer, sprite, Empty, false)
+namespace le
 {
-
+////////////////////////////////////////////////////////////
+SpriteComponent::SpriteComponent(const sf::Vector2f& position, const sf::Texture& texture_pointer, const sf::IntRect& sprite) :
+m_sprite_current(&this->m_sprite_default),
+m_sprite_default(texture_pointer, sprite),
+m_sprite_alt    (std::nullopt),
+m_use_alt       (false)
+{
+	setPosition(position);
 }
 
-SpriteComponent::SpriteComponent(const sf::Vector2f& position, const sf::Texture* texture_pointer, const sf::IntRect& sprite_default, const sf::IntRect& sprite_alt, bool use_alt) :
 
-	m_texture_pointer(texture_pointer),
-	m_sprite_default(new sf::Sprite(*texture_pointer, sprite_default)),
-	m_sprite_alt(sprite_alt == Empty ? nullptr : new sf::Sprite(*texture_pointer, sprite_alt)),
-	m_has_alt(sprite_alt != Empty)
+////////////////////////////////////////////////////////////
+SpriteComponent::SpriteComponent(const sf::Vector2f& position, const sf::Texture& texture_pointer, const sf::IntRect& sprite_default, std::optional<const sf::IntRect> sprite_alt, bool use_alt) :
+m_sprite_current(nullptr),
+m_sprite_default(texture_pointer, sprite_default),
+m_sprite_alt    (sprite_alt ? std::make_optional<sf::Sprite>(texture_pointer, *sprite_alt) : std::nullopt),
+m_use_alt       (false)
 {
-	onTextureUpdated();
 	setPosition(position);
 	setUseAlt(use_alt);
-
-	this->m_sprite_default->setTextureRect(sprite_default);
-
-	if (this->m_has_alt)
-	{
-		this->m_sprite_alt->setTextureRect(sprite_alt);
-	}
 }
 
+
+////////////////////////////////////////////////////////////
 void SpriteComponent::setUseAlt(bool use_alt)
 {
-	this->m_use_alt = this->m_has_alt && use_alt;
-	this->m_sprite_current = this->m_has_alt && use_alt ? this->m_sprite_alt : this->m_sprite_default;
+	this->m_use_alt = this->m_sprite_alt && use_alt;
+	this->m_sprite_current = std::shared_ptr<const sf::Sprite>(this->m_use_alt ? std::to_address(this->m_sprite_alt) : &this->m_sprite_default);
 }
 
-void SpriteComponent::onTextureUpdated()
-{
-	this->m_sprite_default->setTexture(*this->m_texture_pointer, false);
 
-	if (this->m_has_alt)
-	{
-		this->m_sprite_alt->setTexture(*this->m_texture_pointer, false);
-	}
-}
-
+////////////////////////////////////////////////////////////
 void SpriteComponent::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
 	states.transform *= getTransform();
 	target.draw(*this->m_sprite_current, states);
+}
+
 }
--- src/controls/sprite_component.hpp
+++ src/controls/sprite_component.hpp
@@ -1,26 +1,93 @@
-#ifndef SPRITE_COMPONENT_HPP
-#define SPRITE_COMPONENT_HPP
+////////////////////////////////////////////////////////////
+//
+// MIT License
+//
+// Copyright (c) 2022 ZaBlazzingZeif
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
 
+#ifndef LEVEL_EDITOR_SPRITE_COMPONENT_HPP
+#define LEVEL_EDITOR_SPRITE_COMPONENT_HPP
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include <memory>
+#include <optional>
 #include <SFML/Graphics/Sprite.hpp>
 #include <SFML/Graphics/RenderTarget.hpp>
 
+
+namespace le
+{
+////////////////////////////////////////////////////////////
+/// \brief Sprite component representing a rich sf::Sprite wrapper
+///
+////////////////////////////////////////////////////////////
 class SpriteComponent : public sf::Drawable, public sf::Transformable
 {
 public:
-	SpriteComponent(const sf::Vector2f& position, const sf::Texture* texture_pointer, const sf::IntRect& sprite);
-	SpriteComponent(const sf::Vector2f& position, const sf::Texture* texture_pointer, const sf::IntRect& sprite_default, const sf::IntRect& sprite_alt = sf::IntRect(), bool use_alt = false);
+	////////////////////////////////////////////////////////////
+	/// \brief SpriteComponent without the alternative sprite
+	///
+	////////////////////////////////////////////////////////////
+	SpriteComponent(const sf::Vector2f& position, const sf::Texture& texture_pointer, const sf::IntRect& sprite);
 
-	void				setUseAlt(bool use_alt);
-	void				onTextureUpdated();
-	void				draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+	////////////////////////////////////////////////////////////
+	/// \brief SpriteComponent with both sprites
+	///
+	////////////////////////////////////////////////////////////
+	SpriteComponent(const sf::Vector2f& position, const sf::Texture& texture_pointer, const sf::IntRect& sprite_default, std::optional<const sf::IntRect> sprite_alt, bool use_alt = false);
+
+	////////////////////////////////////////////////////////////
+	/// \brief Switch between the default and the alternative sprites.
+	///
+	/// If the alternative sprite was not provided, SpriteComponent uses the default sprite regardless of the provided value.
+	///
+	/// \param use_alt true to use the alternative sprite, false to use the default one.
+	///
+	////////////////////////////////////////////////////////////
+	void								setUseAlt(bool use_alt);
+
+	////////////////////////////////////////////////////////////
+	/// \brief Draw this SpriteComponent to a render target.
+	///
+	/// \param target Render target to draw to
+	/// \param states Current render states
+	///
+	////////////////////////////////////////////////////////////
+	void								draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
 private:
-	const sf::Texture*	m_texture_pointer;
-	sf::Sprite*			m_sprite_current;
-	sf::Sprite*			m_sprite_default;
-	sf::Sprite*			m_sprite_alt;
-	bool				m_has_alt;
-	bool				m_use_alt;
+
+	////////////////////////////////////////////////////////////
+	// Member data
+	////////////////////////////////////////////////////////////
+	std::shared_ptr<const sf::Sprite> m_sprite_current;  //!< Shared pointer to the currenly used sprite. The object pointed by this shall not be modified.
+	sf::Sprite                        m_sprite_default;	 //!< Default sprite
+	std::optional<sf::Sprite>         m_sprite_alt;		 //!< Optional alternative sprite. Is initialized only if the corresponding sf::IntRect was provided
+	bool                              m_use_alt;         //!< Indicates whether to use the alternative sprite or not
 };
 
-#endif
+} //namespace le
+
+
+#endif // LEVEL_EDITOR_SPRITE_COMPONENT_HPP
