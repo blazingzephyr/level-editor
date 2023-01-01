b64f95f9e8424af8616ee68f2b55425c157220fd
TenaciousKirikot <blazerzeif@gmail.com>
2023-01-02 01:07:08 +0700

    Create Button
    Created a clickable button control.

---
+++ src/ui/controls/Button.cpp
@@ -0,0 +1,94 @@
+////////////////////////////////////////////////////////////
+//
+// MIT License
+//
+// Copyright (c) 2023 ZaBlazzingZeif
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include "Button.hpp"
+
+
+namespace le
+{
+////////////////////////////////////////////////////////////
+Button::Button() :
+TextBasedControl::TextBasedControl(),
+m_onReleased(),
+m_onHold()
+{
+};
+
+
+Button::Button(const sf::Vector2f& position, const sf::Vector2f& size, const sf::Texture& texture, 
+const sf::IntRect& spriteDefault, const sf::IntRect& spriteActive, const TextTheme& textTheme, const sf::String& string,
+Event0<Button> onReleased, Event0<Button> onHold, bool enabled) :
+
+TextBasedControl::TextBasedControl(position, sf::Vector2f(0, 0), size, texture, spriteDefault, spriteActive,
+textTheme, string, sf::Vector2f(), false, enabled),
+m_onReleased(onReleased),
+m_onHold(onHold)
+{
+}
+
+////////////////////////////////////////////////////////////
+void Button::onHold()
+{
+	this->m_onHold(*this);
+}
+
+
+////////////////////////////////////////////////////////////
+void Button::onClicked(sf::Event::MouseButtonEvent mouseButton)
+{
+	TextBasedControl::onClicked(mouseButton);
+	this->m_sprite.setUseAlt(true);
+}
+
+
+////////////////////////////////////////////////////////////
+void Button::onReleased(sf::Event::MouseButtonEvent mouseButton)
+{
+	TextBasedControl::onReleased(mouseButton);
+	this->m_sprite.setUseAlt(false);
+	this->m_onReleased(*this);
+}
+
+
+////////////////////////////////////////////////////////////
+void Button::onEntered(sf::Event::MouseMoveEvent mouseMove)
+{
+	TextBasedControl::onEntered(mouseMove);
+	this->m_sprite.setUseAlt(this->m_holding);
+}
+
+
+////////////////////////////////////////////////////////////
+void Button::onLeft(sf::Event::MouseMoveEvent mouseMove)
+{
+	TextBasedControl::onLeft(mouseMove);
+	this->m_sprite.setUseAlt(false);
+}
+
+} //namespace le
---
+++ src/ui/controls/Button.hpp
@@ -0,0 +1,123 @@
+////////////////////////////////////////////////////////////
+//
+// MIT License
+//
+// Copyright (c) 2023 ZaBlazzingZeif
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef LEVEL_EDITOR_BUTTON_HPP
+#define LEVEL_EDITOR_BUTTON_HPP
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include "../interfaces/TextBasedControl.hpp"
+#include "../../utility/Config.hpp"
+
+
+namespace le
+{
+////////////////////////////////////////////////////////////
+/// \brief Clickable button control
+///
+////////////////////////////////////////////////////////////
+class Button : public TextBasedControl
+{
+public:
+
+	////////////////////////////////////////////////////////////
+	/// \brief Default constructor
+	/// 
+	/// Constructs an invalid button
+	/// 
+	////////////////////////////////////////////////////////////
+	Button();
+
+	////////////////////////////////////////////////////////////
+	/// \brief Default constructor
+	/// 
+	/// \param position      Position set to button
+	/// \param size          Size of button
+	/// \param texture       Source texture
+	/// \param spriteDefault Sub-rectangle of the texture to assign to the default sprite
+	/// \param spriteActive  Sub-rectangle of the texture to assign to the active sprite
+	/// \param textTheme     Text theme
+	/// \param string        String assigned to the text
+	/// \param onReleased    Event raised when this button is released
+	/// \param onHold        Event raised when this button is held
+	/// \param enable        Enable this button
+	/// 
+	////////////////////////////////////////////////////////////
+	Button(const sf::Vector2f& position, const sf::Vector2f& size, const sf::Texture& texture, const sf::IntRect& spriteDefault,
+	const sf::IntRect& spriteActive, const TextTheme& textTheme, const sf::String& string = sf::String(),
+	Event0<Button> onReleased = Event0<Button>(), Event0<Button> onHold = Event0<Button>(), bool enabled = true);
+
+
+private:
+	Event0<Button> m_onReleased; //!< Event raised when this button is released
+	Event0<Button> m_onHold;     //!< Event raised when this button is held
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event continuously triggered when the mouse button is held
+	///
+	////////////////////////////////////////////////////////////
+	void onHold() override;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event triggered when a mouse button is pressed
+	///        while the button is being hovered
+	/// 
+	/// \param mouseButton Mouse button event parameters
+	///
+	////////////////////////////////////////////////////////////
+	void onClicked(sf::Event::MouseButtonEvent mouseButton) override;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event triggered when a mouse button is released
+	///        while the button is being hovered
+	/// 
+	/// \param mouseButton Mouse button event parameters
+	///
+	////////////////////////////////////////////////////////////
+	void onReleased(sf::Event::MouseButtonEvent mouseButton) override;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event triggered when the mouse cursor enters the area of the button
+	///
+	/// \param mouseMove Mouse move event parameters
+	///
+	////////////////////////////////////////////////////////////
+	void onEntered(sf::Event::MouseMoveEvent mouseMove) override;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event triggered when the mouse cursor leaves the area of the button
+	///
+	/// \param mouseMove Mouse move event parameters
+	///
+	////////////////////////////////////////////////////////////
+	void onLeft(sf::Event::MouseMoveEvent mouseMove) override;
+};
+
+} //namespace le
+
+
+#endif // LEVEL_EDITOR_BUTTON_HPP
