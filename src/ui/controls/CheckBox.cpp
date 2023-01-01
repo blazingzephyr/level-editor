99aa4e3cd5a1ffa34a7870f37ea993654dd74aff
TenaciousKirikot <blazerzeif@gmail.com>
2023-01-01 18:29:42 +0700

    Create CheckBox

---
+++ src/ui/controls/CheckBox.cpp
@@ -0,0 +1,126 @@
+////////////////////////////////////////////////////////////
+//
+// MIT License
+//
+// Copyright (c) 2022 ZaBlazzingZeif
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include "CheckBox.hpp"
+
+
+namespace le
+{
+////////////////////////////////////////////////////////////
+CheckBox::CheckBox() :
+SpriteBasedControl::SpriteBasedControl(),
+m_onCheckedChanged(),
+m_isChecked(false)
+{
+};
+
+
+////////////////////////////////////////////////////////////
+CheckBox::CheckBox(const sf::Vector2f& position, const sf::Vector2f& size, const sf::Texture& texture, const sf::IntRect& spriteUnchecked,
+const sf::IntRect& spriteChecked, Event<CheckBox, bool> onCheckedChanged, bool isChecked, bool enabled) :
+SpriteBasedControl::SpriteBasedControl(position, size, texture, spriteUnchecked, spriteChecked, false, enabled),
+m_onCheckedChanged(onCheckedChanged)
+{
+	setPosition(position);
+	setChecked(isChecked, false);
+}
+
+
+////////////////////////////////////////////////////////////
+bool CheckBox::getChecked() const
+{
+	return m_isChecked;
+}
+
+
+////////////////////////////////////////////////////////////
+void CheckBox::setChecked(bool checked, bool raiseEvent)
+{
+	if (this->m_isChecked != checked)
+	{
+		this->m_isChecked = checked;
+		this->m_sprite.setUseAlt(checked);
+
+		if (raiseEvent)
+		{
+			this->m_onCheckedChanged(*this, checked);
+		}
+	}
+}
+
+
+////////////////////////////////////////////////////////////
+void CheckBox::setSprite(bool swap)
+{
+	if (this->m_enabled && this->m_holding)
+	{
+		this->m_sprite.setUseAlt(this->m_isChecked == swap);
+	}
+}
+
+
+////////////////////////////////////////////////////////////
+void CheckBox::onScrolledControl(sf::Event::MouseWheelScrollEvent mouseWheelScroll)
+{
+	setChecked(!this->m_isChecked);
+}
+
+
+////////////////////////////////////////////////////////////
+void CheckBox::onClicked(sf::Event::MouseButtonEvent mouseButton)
+{
+	SpriteBasedControl::onClicked(mouseButton);
+	setSprite(false);
+}
+
+
+////////////////////////////////////////////////////////////
+void CheckBox::onReleased(sf::Event::MouseButtonEvent mouseButton)
+{
+	SpriteBasedControl::onReleased(mouseButton);
+	setChecked(!this->m_isChecked);
+}
+
+
+////////////////////////////////////////////////////////////
+void CheckBox::onEntered(sf::Event::MouseMoveEvent mouseMove)
+{
+	SpriteBasedControl::onEntered(mouseMove);
+	setSprite(false);
+}
+
+
+////////////////////////////////////////////////////////////
+void CheckBox::onLeft(sf::Event::MouseMoveEvent mouseMove)
+{
+	SpriteBasedControl::onLeft(mouseMove);
+	setSprite(true);
+}
+
+} //namespace le
---
+++ src/ui/controls/CheckBox.hpp
@@ -0,0 +1,144 @@
+////////////////////////////////////////////////////////////
+//
+// MIT License
+//
+// Copyright (c) 2022 ZaBlazzingZeif
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef LEVEL_EDITOR_CHECK_BOX_HPP
+#define LEVEL_EDITOR_CHECK_BOX_HPP
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include "../interfaces/SpriteBasedControl.hpp"
+#include "../../utility/Config.hpp"
+
+
+namespace le
+{
+////////////////////////////////////////////////////////////
+/// \brief Check box control
+///
+////////////////////////////////////////////////////////////
+class CheckBox : public SpriteBasedControl
+{
+public:
+
+	////////////////////////////////////////////////////////////
+	/// \brief Default constructor
+	/// 
+	/// Constructs an invalid check box
+	/// 
+	////////////////////////////////////////////////////////////
+	CheckBox();
+
+	////////////////////////////////////////////////////////////
+	/// \brief Default constructor
+	/// 
+	/// \param position        Position set to check box
+	/// \param size            Size of check box
+	/// \param texture         Source texture
+	/// \param spriteUnchecked Sub-rectangle of the texture to assign to the unchecked sprite
+	/// \param spriteChecked   Sub-rectangle of the texture to assign to the checked sprite
+	/// \param isChecked       Is check box checked
+	/// \param enable          Enable this check box
+	/// 
+	////////////////////////////////////////////////////////////
+	CheckBox(const sf::Vector2f& position, const sf::Vector2f& size, const sf::Texture& texture, const sf::IntRect& spriteUnchecked,
+	const sf::IntRect& spriteChecked, Event<CheckBox, bool> onCheckedChanged = Empty<CheckBox, bool>, bool isChecked = false, bool enabled = true);
+
+	////////////////////////////////////////////////////////////
+	/// \brief Check state of the box
+	/// 
+	////////////////////////////////////////////////////////////
+	bool getChecked() const;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Sets check state of the box
+	///
+	/// \param checked    New check state
+	/// \param raiseEvent Raise OnCheckedChanged event
+	/// 
+	////////////////////////////////////////////////////////////
+	void setChecked(bool checked, bool raiseEvent = true);
+
+private:
+	Event<CheckBox, bool> m_onCheckedChanged; //!< Event raised whenever m_isChecked changes
+	bool                  m_isChecked;        //!< Check state of the box
+
+	////////////////////////////////////////////////////////////
+	/// \brief Sets default or alternate sprite for use
+	///
+	/// \param swap Swap the sprite
+	///
+	////////////////////////////////////////////////////////////
+	void setSprite(bool swap);
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event triggered when the mouse wheel is scrolled 
+	///        while the check box is being hovered
+	/// 
+	/// \param mouseWheelScroll Mouse wheel event parameters
+	///
+	////////////////////////////////////////////////////////////
+	void onScrolledControl(sf::Event::MouseWheelScrollEvent mouseWheelScroll) override;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event triggered when a mouse button is pressed
+	///        while the check box is being hovered
+	/// 
+	/// \param mouseButton Mouse button event parameters
+	///
+	////////////////////////////////////////////////////////////
+	void onClicked(sf::Event::MouseButtonEvent mouseButton) override;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event triggered when a mouse button is released
+	///        while the check box is being hovered
+	/// 
+	/// \param mouseButton Mouse button event parameters
+	///
+	////////////////////////////////////////////////////////////
+	void onReleased(sf::Event::MouseButtonEvent mouseButton) override;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event triggered when the mouse cursor enters the area of the check box
+	///
+	/// \param mouseMove Mouse move event parameters
+	///
+	////////////////////////////////////////////////////////////
+	void onEntered(sf::Event::MouseMoveEvent mouseMove) override;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event triggered when the mouse cursor leaves the area of the check box
+	///
+	/// \param mouseMove Mouse move event parameters
+	///
+	////////////////////////////////////////////////////////////
+	void onLeft(sf::Event::MouseMoveEvent mouseMove) override;
+};
+
+} //namespace le
+
+
+#endif // LEVEL_EDITOR_CHECK_BOX_HPP
