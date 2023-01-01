d53ad7c0376fed4bdde75e1c28c2b6be1679b4b8
TenaciousKirikot <blazerzeif@gmail.com>
2023-01-01 18:03:39 +0700

    Create SpriteBasedControl
    Created an abstract class for all controls with one sprite component.

---
+++ src/ui/interfaces/SpriteBasedControl.cpp
@@ -0,0 +1,92 @@
+////////////////////////////////////////////////////////////
+//
+// MIT License
+//
+// Copyright (c) 2022 ZaBlazzingZeif
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include "SpriteBasedControl.hpp"
+
+
+namespace le
+{
+////////////////////////////////////////////////////////////
+SpriteBasedControl::SpriteBasedControl() :
+Control::Control(),
+m_shader(nullptr),
+m_sprite()
+{
+}
+
+
+////////////////////////////////////////////////////////////
+SpriteBasedControl::SpriteBasedControl(sf::Vector2f position, sf::Vector2f size, const sf::Texture& texture, 
+const sf::IntRect& spriteDefault, const sf::IntRect& spriteAlt, bool useAlt, bool enabled) :
+Control::Control(position, size, enabled),
+m_shader(nullptr),
+m_sprite(sf::Vector2f(), texture, spriteDefault, spriteAlt, useAlt)
+{
+}
+
+
+////////////////////////////////////////////////////////////
+void SpriteBasedControl::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	states.transform *= getTransform();
+	states.shader = this->m_shader;
+	target.draw(this->m_sprite, states);
+}
+
+
+////////////////////////////////////////////////////////////
+void SpriteBasedControl::onClicked(sf::Event::MouseButtonEvent mouseButton)
+{
+	printf("SpriteBasedControl::onClicked IS NOT IMPLEMENTED\n");
+}
+
+
+////////////////////////////////////////////////////////////
+void SpriteBasedControl::onReleased(sf::Event::MouseButtonEvent mouseButton)
+{
+	printf("SpriteBasedControl::onReleased IS NOT IMPLEMENTED\n");
+}
+
+
+////////////////////////////////////////////////////////////
+void SpriteBasedControl::onEntered(sf::Event::MouseMoveEvent mouseMove)
+{
+	this->m_shader = this->m_enabled ? nullptr_t() : nullptr_t();
+	printf("SpriteBasedControl::onEntered IS NOT IMPLEMENTED\n");
+}
+
+
+////////////////////////////////////////////////////////////
+void SpriteBasedControl::onLeft(sf::Event::MouseMoveEvent mouseMove)
+{
+	this->m_shader = this->m_enabled ? nullptr : nullptr_t();
+	printf("SpriteBasedControl::onLeft IS NOT IMPLEMENTED\n");
+}
+
+} //namespace le
---
+++ src/ui/interfaces/SpriteBasedControl.hpp
@@ -0,0 +1,130 @@
+////////////////////////////////////////////////////////////
+//
+// MIT License
+//
+// Copyright (c) 2022 ZaBlazzingZeif
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef LEVEL_EDITOR_SPRITE_BASED_CONTROL_HPP
+#define LEVEL_EDITOR_SPRITE_BASED_CONTROL_HPP
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include "Control.hpp"
+#include "../components/SpriteComponent.hpp"
+#include <SFML/Graphics/Shader.hpp>
+
+
+namespace le
+{
+////////////////////////////////////////////////////////////
+/// \brief Abstract class, representing a user control with SpriteComponent
+///
+////////////////////////////////////////////////////////////
+class SpriteBasedControl : public Control 
+{
+public:
+
+	////////////////////////////////////////////////////////////
+	/// \brief Default constructor
+	/// 
+	/// This constructor creates an invalid sprite-based control
+	/// 
+	////////////////////////////////////////////////////////////
+	SpriteBasedControl();
+
+	////////////////////////////////////////////////////////////
+	/// \brief Default constructor
+	/// 
+	/// \param position      Position set to control
+	/// \param size          Size of control
+	/// \param texture       Source texture
+	/// \param spriteDefault Sub-rectangle of the texture to assign to the default sprite
+	/// \param spriteAlt     Sub-rectangle of the texture to assign to the alternative sprite
+	/// \param useAlt        Use alternative sprite instead of the default one
+	/// \param enable        Enable this control
+	/// 
+	////////////////////////////////////////////////////////////
+	SpriteBasedControl(sf::Vector2f position, sf::Vector2f size, const sf::Texture& texture,
+	const sf::IntRect& spriteDefault, const sf::IntRect& spriteAlt, bool useAlt, bool enabled);
+
+	////////////////////////////////////////////////////////////
+	/// \brief Virtual destructor
+	///
+	////////////////////////////////////////////////////////////
+	virtual ~SpriteBasedControl() {}
+
+	////////////////////////////////////////////////////////////
+	/// \brief Draw this sprite-based control to a render target.
+	///
+	/// \param target Render target to draw to
+	/// \param states Current render states
+	///
+	////////////////////////////////////////////////////////////
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+protected:
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event triggered when a mouse button is pressed while hovering
+	///
+	/// \param mouseButton Mouse button event parameters
+	///
+	////////////////////////////////////////////////////////////
+	virtual void onClicked(sf::Event::MouseButtonEvent mouseButton) override;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event triggered when a mouse button is released pressed while hovering
+	///
+	/// \param mouseButton Mouse button event parameters
+	///
+	////////////////////////////////////////////////////////////
+	virtual void onReleased(sf::Event::MouseButtonEvent mouseButton) override;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event triggered when the mouse cursor enters the area of the control
+	///
+	/// \param mouseMove Mouse move event parameters
+	///
+	////////////////////////////////////////////////////////////
+	virtual void onEntered(sf::Event::MouseMoveEvent mouseMove) override;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event triggered when the mouse cursor leaves the area of the control
+	///
+	/// \param mouseMove Mouse move event parameters
+	///
+	////////////////////////////////////////////////////////////
+	virtual void onLeft(sf::Event::MouseMoveEvent mouseMove) override;
+
+	////////////////////////////////////////////////////////////
+	// Member data
+	////////////////////////////////////////////////////////////
+	SpriteComponent m_sprite;   //!< Sprite drawn in SpriteBasedControl::draw
+	const sf::Shader* m_shader; //!< Shader applied to sprite in SpriteBasedControl::draw
+};
+
+} //namespace le
+
+
+#endif // LEVEL_EDITOR_SPRITE_BASED_CONTROL_HPP
