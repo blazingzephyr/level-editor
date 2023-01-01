afcfefc03d0aa916f78bd9f41b65c7c83913805a
TenaciousKirikot <blazerzeif@gmail.com>
2023-01-01 17:38:24 +0700

    Create Config.hpp
    Created a file for misc stuff.

---
+++ src/utility/Config.hpp
@@ -0,0 +1,55 @@
+////////////////////////////////////////////////////////////
+//
+// MIT License
+//
+// Copyright (c) 2022 ZaBlazzingZeif
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef LEVEL_EDITOR_CONFIG_HPP
+#define LEVEL_EDITOR_CONFIG_HPP
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include <functional>
+
+
+namespace le
+{
+////////////////////////////////////////////////////////////
+/// \brief Alias for std::function representing an event with an argument raised within sender
+///
+////////////////////////////////////////////////////////////
+template <typename T, typename U>
+using Event = std::function<void(T& sender, U arg)>;
+
+////////////////////////////////////////////////////////////
+/// \brief Alias for an empty event
+///
+////////////////////////////////////////////////////////////
+template <typename T, typename U>
+Event<T, U> Empty = Event<T, U>();
+
+} // namespace le
+
+
+#endif // LEVEL_EDITOR_CONFIG_HPP
