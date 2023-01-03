67dd71232bf2639a95316522ef6d5aaa3084d438
TenaciousKirikot <blazerzeif@gmail.com>
2023-01-03 12:08:29 +0700

    Create Slider
    Added Slider control

---
+++ src/ui/controls/Slider.hpp
@@ -0,0 +1,208 @@
+////////////////////////////////////////////////////////////
+//
+// MIT License
+//
+// Copyright (c) 2023 ZaBlazzingZeif
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef LEVEL_EDITOR_SLIDER_HPP
+#define LEVEL_EDITOR_SLIDER_HPP
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include "../interfaces/SpriteBasedControl.hpp"
+#include "../../utility/Config.hpp"
+
+
+namespace le
+{
+////////////////////////////////////////////////////////////
+/// \brief Slider control
+///
+////////////////////////////////////////////////////////////
+template <typename T>
+requires std::is_arithmetic_v<T>
+class Slider : public SpriteBasedControl
+{
+public:
+
+	////////////////////////////////////////////////////////////
+	/// \brief Default constructor
+	/// 
+	/// Constructs an empty slider
+	/// 
+	////////////////////////////////////////////////////////////
+	Slider();
+
+	////////////////////////////////////////////////////////////
+	/// \brief Default constructor
+	/// 
+	/// \param position          Position set to check box
+	/// \param size              Size of check box
+	/// \param thumbSize         Size of slider's thumb
+	/// \param texture           Source texture
+	/// \param spriteDefault     Sub-rectangle of the texture to assign to the default sprite
+	/// \param spriteActive      Sub-rectangle of the texture to assign to the active sprite
+	/// \param thumb             Sub-rectangle of the texture to assign to the thumb
+	/// \param isVertical        true for vertical slider, false for horizontal slider
+	/// \param min               Minimal accepted value
+	/// \param max               Maximum accepted value
+	/// \param value             Value to set to slider
+	/// \param scrollMultiplier  Affects the amount of change on scrolling
+	/// \param onValueChanged    Event raised when slider's value has changed
+	/// \param onMaxValueChanged Event raised when slider's max value has changed
+	/// \param enable            Enable this check box
+	/// 
+	////////////////////////////////////////////////////////////
+	Slider(const sf::Vector2f& position, const sf::Vector2f& size, const sf::Vector2f& thumbSize, const sf::Texture& texture,
+	const sf::IntRect& spriteDefault, const sf::IntRect& spriteActive, const sf::IntRect& thumb, bool isVertical,
+	T min, T max, T value, float scrollMultiplier = 1.f, Event1<Slider<T>, T> onValueChanged = Event1<Slider<T>, T>(),
+	Event1<Slider<T>, T> onMaxValueChanged = Event1<Slider<T>, T>(), bool enabled = true);
+
+	////////////////////////////////////////////////////////////
+	/// \brief Get slider's value
+	/// 
+	////////////////////////////////////////////////////////////
+	T getValue() const;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Set percentage
+	///
+	/// \param percentage New percentage
+	/// \param raiseEvent Raise OnValueChanged event
+	/// 
+	////////////////////////////////////////////////////////////
+	void setPercentage(float percentage, bool raiseEvent = true);
+
+	////////////////////////////////////////////////////////////
+	/// \brief Set value
+	///
+	/// \param value      New value
+	/// \param raiseEvent Raise OnValueChanged event
+	/// 
+	////////////////////////////////////////////////////////////
+	void setValue(T value, bool raiseEvent = true);
+
+	////////////////////////////////////////////////////////////
+	/// \brief Set max value
+	///
+	/// \param max        New max value
+	/// \param raiseEvent Raise OnMaxValueChanged event
+	/// 
+	////////////////////////////////////////////////////////////
+	void setMax(T max, bool raiseEvent = true);
+
+	////////////////////////////////////////////////////////////
+	/// \brief Draw the slider to a render target
+	///
+	/// \param target Render target to draw to
+	/// \param states Current render states
+	///
+	////////////////////////////////////////////////////////////
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+private:
+
+	////////////////////////////////////////////////////////////
+	/// \brief Move thumb on mouse actions
+	///
+	////////////////////////////////////////////////////////////
+	void moveThumb(float x, float y);
+
+	////////////////////////////////////////////////////////////
+	/// \brief Update thumb's position
+	///
+	////////////////////////////////////////////////////////////
+	void updateThumbPosition();
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event triggered when the mouse wheel is scrolled 
+	///        while the check box is being hovered
+	/// 
+	/// \param mouseWheelScroll Mouse wheel event parameters
+	///
+	////////////////////////////////////////////////////////////
+	void onScrolledControl(sf::Event::MouseWheelScrollEvent mouseWheelScroll) override;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event triggered when a mouse button is pressed
+	///
+	/// \param mouseButton Mouse button event parameters
+	///
+	////////////////////////////////////////////////////////////
+	void onPressed(sf::Event::MouseButtonEvent mouseButton) override;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event triggered when a mouse button is pressed
+	///        while the check box is being hovered
+	/// 
+	/// \param mouseButton Mouse button event parameters
+	///
+	////////////////////////////////////////////////////////////
+	void onClicked(sf::Event::MouseButtonEvent mouseButton) override;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event triggered when a mouse button is released
+	/// 
+	/// \param mouseButton Mouse button event parameters
+	///
+	////////////////////////////////////////////////////////////
+	void onReleased(sf::Event::MouseButtonEvent mouseButton) override;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event triggered when the mouse cursor moves
+	///        while the control is being hovered
+	/// 
+	/// \param mouseMove Mouse move event parameters
+	///
+	////////////////////////////////////////////////////////////
+	void onMovedControl(sf::Event::MouseMoveEvent mouseMove) override;
+
+	////////////////////////////////////////////////////////////
+	// Member data
+	////////////////////////////////////////////////////////////
+	Event1<Slider<T>, T> m_onValueChanged;    //!< Event raised when m_value changes
+	Event1<Slider<T>, T> m_onMaxValueChanged; //!< Event raised when m_value changes
+	SpriteComponent      m_thumb;             //!< Slider thumb
+	sf::Vector2f         m_thumbSize;         //!< Thumb size
+	bool                 m_isVertical;        //!< Enable vertical mode
+	T                    m_min;               //!< Minimal accepted value
+	T                    m_max;               //!< Maximum accepted value
+	T                    m_range;             //!< Range of accepted values
+	T                    m_value;             //!< Current value
+	float                m_percentage;        //!< Value percentage
+	float                m_scrollMultiplier;  //!< Affects the amount of change on scrolling
+	bool                 m_isScrolling;       //!< Is user scrolling
+};
+
+using ByteSlider = Slider<uint8_t>;
+using ShortSlider = Slider<uint16_t>;
+using UintSlider = Slider<uint32_t>;
+using IntSlider = Slider<int32_t>;
+using FloatSlider = Slider<float_t>;
+
+} //namespace le
+
+
+#include "Slider.inl"
+#endif // LEVEL_EDITOR_SLIDER_HPP
---
+++ src/ui/controls/Slider.inl
@@ -0,0 +1,263 @@
+////////////////////////////////////////////////////////////
+//
+// MIT License
+//
+// Copyright (c) 2023 ZaBlazzingZeif
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef LEVEL_EDITOR_SLIDER_INL
+#define LEVEL_EDITOR_SLIDER_INL
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include "Slider.hpp"
+
+
+namespace le
+{
+////////////////////////////////////////////////////////////
+template<typename T>
+requires std::is_arithmetic_v<T>
+inline Slider<T>::Slider() :
+
+SpriteBasedControl::SpriteBasedControl(),
+m_onValueChanged(),
+m_onMaxValueChanged(),
+m_thumb(),
+m_thumbSize(0.f, 0.f),
+m_isVertical(false),
+m_min(0),
+m_max(0),
+m_range(0),
+m_value(0),
+m_percentage(0),
+m_scrollMultiplier(0.f),
+m_isScrolling(false)
+{
+}
+
+
+////////////////////////////////////////////////////////////
+template<typename T>
+requires std::is_arithmetic_v<T>
+inline Slider<T>::Slider(const sf::Vector2f& position, const sf::Vector2f& size, const sf::Vector2f& thumbSize, const sf::Texture& texture, const sf::IntRect& spriteDefault,
+const sf::IntRect& spriteActive, const sf::IntRect& thumb, bool isVertical, T min, T max, T value, float scrollMultiplier,
+Event1<Slider<T>, T> onValueChanged, Event1<Slider<T>, T> onMaxValueChanged, 
+bool enabled) :
+
+SpriteBasedControl::SpriteBasedControl(position, size, texture, spriteDefault, spriteActive, false, enabled),
+m_onValueChanged(onValueChanged),
+m_onMaxValueChanged(onMaxValueChanged),
+m_thumb(sf::Vector2f(), texture, thumb),
+m_thumbSize(thumbSize),
+m_isVertical(isVertical),
+m_min(min),
+m_max(max),
+m_range(max - min),
+m_value(value),
+m_percentage((value - min) / static_cast<float>(max - min)),
+m_scrollMultiplier(scrollMultiplier),
+m_isScrolling(false)
+{
+	this->m_thumb.setOrigin(thumbSize.x / 2.f, thumbSize.x / 2.f);
+	updateThumbPosition();
+}
+
+
+////////////////////////////////////////////////////////////
+template<typename T>
+requires std::is_arithmetic_v<T>
+inline T Slider<T>::getValue() const
+{
+	return this->m_value;
+}
+
+
+////////////////////////////////////////////////////////////
+template<typename T>
+requires std::is_arithmetic_v<T>
+inline void Slider<T>::setPercentage(float percentage, bool raiseEvent)
+{
+	if (percentage >= 0.f && percentage <= 1.f)
+	{
+		this->m_percentage = percentage;
+		this->m_value = this->m_min;
+		
+		float value = this->m_range * percentage;
+		this->m_value += std::is_integral_v<T> ? std::round(value) : value;
+		updateThumbPosition();
+
+		if (raiseEvent)
+		{
+			this->m_onValueChanged(*this, this->m_value);
+		}
+	}
+}
+
+
+////////////////////////////////////////////////////////////
+template<typename T>
+requires std::is_arithmetic_v<T>
+inline void Slider<T>::setValue(T value, bool raiseEvent)
+{
+	if (value >= this->m_min && value <= this->m_max)
+	{
+		this->m_value = value;
+		this->m_percentage = (value - this->m_min) / static_cast<float>(this->m_range);
+		updateThumbPosition();
+
+		if (raiseEvent)
+		{
+			this->m_onValueChanged(*this, this->m_value);
+		}
+	}
+}
+
+
+////////////////////////////////////////////////////////////
+template<typename T>
+requires std::is_arithmetic_v<T>
+inline void Slider<T>::setMax(T max, bool raiseEvent)
+{
+	this->m_max = max;
+	this->m_range = max - this->m_min;
+	this->m_percentage = (this->m_value - this->m_min) / static_cast<float>(this->m_range);
+	updateThumbPosition(m_percentage);
+
+	if (raiseEvent)
+	{
+		this->m_onMaxValueChanged(*this, this->m_max);
+	}
+}
+
+
+////////////////////////////////////////////////////////////
+template<typename T>
+requires std::is_arithmetic_v<T>
+inline void Slider<T>::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	SpriteBasedControl::draw(target, states);
+	states.transform *= getTransform();
+	target.draw(this->m_thumb, states);
+}
+
+
+////////////////////////////////////////////////////////////
+template<typename T>
+requires std::is_arithmetic_v<T>
+inline void Slider<T>::moveThumb(float x, float y)
+{
+	sf::Vector2f sliderPosition = this->getPosition();
+	sf::Vector2f thumbPosition = this->m_thumb.getPosition();
+
+	bool containsSlider = m_isVertical ?
+		sliderPosition.y <= y <= sliderPosition.y + this->m_size.y :
+		sliderPosition.x <= x <= sliderPosition.x + this->m_size.x;
+
+	bool containsThumb = x >= thumbPosition.x &&
+		                 x <= thumbPosition.x + this->m_size.x &&
+		                 y >= thumbPosition.y &&
+		                 y <= thumbPosition.y + this->m_size.y;
+
+	if (containsSlider)
+	{
+		setPercentage(this->m_isVertical ?
+			(y - sliderPosition.y) / this->m_size.y :
+			(x - sliderPosition.x) / this->m_size.x
+		);
+	}
+	else if (containsThumb)
+	{
+		setValue(this->m_isVertical ?
+			y <= sliderPosition.y :
+			x <= sliderPosition.x ?
+				this->m_min : this->m_max
+		);
+	}
+}
+
+
+////////////////////////////////////////////////////////////
+template<typename T>
+requires std::is_arithmetic_v<T>
+inline void Slider<T>::updateThumbPosition()
+{
+	float x = this->m_isVertical ? 0.5f : this->m_percentage;
+	float y = this->m_isVertical ? this->m_percentage : 0.5f;
+	this->m_thumb.setPosition(this->m_size.x * x, this->m_size.y * y);
+}
+
+
+////////////////////////////////////////////////////////////
+template<typename T>
+requires std::is_arithmetic_v<T>
+inline void Slider<T>::onScrolledControl(sf::Event::MouseWheelScrollEvent mouseWheelScroll)
+{
+	setPercentage(this->m_percentage - mouseWheelScroll.delta / 100 * this->m_scrollMultiplier);
+}
+
+
+////////////////////////////////////////////////////////////
+template<typename T>
+requires std::is_arithmetic_v<T>
+inline void Slider<T>::onPressed(sf::Event::MouseButtonEvent mouseButton)
+{
+	this->m_isScrolling = false;
+}
+
+
+////////////////////////////////////////////////////////////
+template<typename T>
+requires std::is_arithmetic_v<T>
+inline void Slider<T>::onClicked(sf::Event::MouseButtonEvent mouseButton)
+{
+	this->m_sprite.setUseAlt(true);
+}
+
+
+////////////////////////////////////////////////////////////
+template<typename T>
+requires std::is_arithmetic_v<T>
+inline void Slider<T>::onReleased(sf::Event::MouseButtonEvent mouseButton)
+{
+	this->m_sprite.setUseAlt(false);
+
+	if (!this->m_isScrolling)
+	{
+		moveThumb(mouseButton.x, mouseButton.y);
+	}
+}
+
+
+////////////////////////////////////////////////////////////
+template<typename T>
+requires std::is_arithmetic_v<T>
+inline void Slider<T>::onMovedControl(sf::Event::MouseMoveEvent mouseMove)
+{
+	moveThumb(mouseMove.x, mouseMove.y);
+}
+
+} //namespace le
+
+
+#endif // LEVEL_EDITOR_SLIDER_INL
