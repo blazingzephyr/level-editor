c48d5e5521280b26502dfe6b277064a738ef1f57
TenaciousKirikot <blazerzeif@gmail.com>
2023-01-03 22:26:05 +0700

    Create ``ComboBox``
    Added a new control.

---
+++ src/ui/controls/ComboBox.cpp
@@ -0,0 +1,210 @@
+////////////////////////////////////////////////////////////
+//
+// MIT License
+//
+// Copyright (c) 2023 ZaBlazzingZeif
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include "ComboBox.hpp"
+
+
+namespace le
+{
+////////////////////////////////////////////////////////////
+ComboBox::ComboBox() :
+Control::Control(),
+m_text(),
+m_onItemChanged(),
+m_buttonLeft(),
+m_buttonRight(),
+m_items(),
+m_item(),
+m_index(0),
+m_clock(),
+m_restart(false),
+m_elapsed(0),
+m_scrollTime(0),
+m_holdingButton(false)
+{
+}
+
+
+////////////////////////////////////////////////////////////
+ComboBox::ComboBox(const sf::Vector2f& position, const sf::Vector2f& size, const sf::Vector2f& buttonSize, const sf::Texture& texture,
+const sf::IntRect& spriteDefault, const sf::IntRect& spriteActive, const TextStyle* style, const TextTheme* theme,
+const Strings* strings, const Items& items, size_t index, int scrollTime, Event1<ComboBox, size_t> onItemChanged, bool enabled) :
+
+Control::Control(position, size, enabled),
+m_buttonLeft(sf::Vector2f(buttonSize.x * 1, size.y / 2), buttonSize, texture, spriteDefault, spriteActive,
+theme, strings, L"", [this](Button&) { onReleasedButton(-1); }, [this](Button&) { onHoldButton(-1); }, enabled),
+
+m_buttonRight(sf::Vector2f(size.x - buttonSize.x, size.y / 2), buttonSize, texture, spriteDefault, spriteActive,
+theme, strings, L"", [this](Button&) { onReleasedButton(1); }, [this](Button&) { onHoldButton(1); }, enabled),
+
+m_text(sf::Vector2f(), sf::Vector2u(size), style, strings, items[index]),
+m_onItemChanged(onItemChanged),
+m_items(items),
+m_clock(),
+m_restart(false),
+m_elapsed(0),
+m_scrollTime(scrollTime),
+m_holdingButton(false)
+{
+	this->m_buttonLeft.setOrigin(0, buttonSize.y / 2);
+	this->m_buttonLeft.setScale(-1, 1);
+	this->m_buttonLeft.setParent(this);
+
+	this->m_buttonRight.setOrigin(0, buttonSize.y / 2);
+	this->m_buttonRight.setParent(this);
+
+	setIndex(index, false);
+}
+
+
+////////////////////////////////////////////////////////////
+sf::String ComboBox::getItem() const
+{
+	return this->m_item;
+}
+
+
+////////////////////////////////////////////////////////////
+bool ComboBox::getIndex() const
+{
+	return this->m_index;
+}
+
+
+////////////////////////////////////////////////////////////
+void ComboBox::setItems(Items items, size_t new_index, bool raiseEvent)
+{
+	this->m_items = items;
+	setIndex(new_index, raiseEvent);
+}
+
+
+////////////////////////////////////////////////////////////
+void ComboBox::setIndex(size_t index, bool raiseEvent)
+{
+	if (index < this->m_items.size())
+	{
+		this->m_index = index;
+		this->m_item = this->m_items[index];
+		this->m_text.setString(this->m_item);
+
+		if (raiseEvent)
+		{
+			this->m_onItemChanged(*this, index);
+		}
+	}
+}
+
+
+////////////////////////////////////////////////////////////
+void ComboBox::setEnabled(bool enabled)
+{
+	Control::setEnabled(enabled);
+	this->m_buttonLeft.setEnabled(enabled);
+	this->m_buttonRight.setEnabled(enabled);
+}
+
+
+////////////////////////////////////////////////////////////
+void ComboBox::update()
+{
+	Control::update();
+	this->m_buttonLeft.update();
+	this->m_buttonRight.update();
+}
+
+
+////////////////////////////////////////////////////////////
+bool ComboBox::onWindowEvent(sf::RenderWindow& window, sf::Event event)
+{
+	bool isAccepted = Control::onWindowEvent(window, event);
+	if (isAccepted)
+	{
+		this->m_buttonLeft.onWindowEvent(window, event);
+		this->m_buttonRight.onWindowEvent(window, event);
+	}
+
+	return isAccepted;
+}
+
+
+////////////////////////////////////////////////////////////
+void ComboBox::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	states.transform *= getTransform();
+	target.draw(this->m_text, states);
+	target.draw(this->m_buttonLeft, states);
+	target.draw(this->m_buttonRight, states);
+}
+
+
+////////////////////////////////////////////////////////////
+void ComboBox::onReleasedButton(uint8_t summand)
+{
+	this->m_elapsed = m_clock.getElapsedTime().asMilliseconds();
+	if (!m_holdingButton || this->m_elapsed >= this->m_scrollTime)
+	{
+		this->m_holdingButton = false;
+		m_clock.restart();
+		moveIndex(summand);
+	}
+}
+
+
+////////////////////////////////////////////////////////////
+void ComboBox::onHoldButton(uint8_t summand)
+{	
+	this->m_holdingButton = true;
+	this->m_elapsed = m_clock.getElapsedTime().asMilliseconds();
+
+	if (this->m_elapsed >= this->m_scrollTime)
+	{
+		m_clock.restart();
+		moveIndex(summand);
+	}
+}
+
+
+////////////////////////////////////////////////////////////
+void ComboBox::moveIndex(int8_t summand)
+{
+	long long new_index = this->m_index + summand;
+	if (new_index < 0)
+	{
+		new_index = m_items.size() - 1;
+	}
+	else if (new_index == m_items.size())
+	{
+		new_index = 0;
+	}
+
+	setIndex(new_index);
+}
+
+} //namespace le
---
+++ src/ui/controls/ComboBox.hpp
@@ -0,0 +1,188 @@
+////////////////////////////////////////////////////////////
+//
+// MIT License
+//
+// Copyright (c) 2023 ZaBlazzingZeif
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+////////////////////////////////////////////////////////////
+
+#ifndef LEVEL_EDITOR_COMBOBOX_HPP
+#define LEVEL_EDITOR_COMBOBOX_HPP
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include "Button.hpp"
+#include <SFML/System/Clock.hpp>
+
+
+namespace le
+{
+////////////////////////////////////////////////////////////
+/// \brief Combo box control
+///
+////////////////////////////////////////////////////////////
+using Items = std::vector<sf::String>;
+class ComboBox : public Control
+{
+public:
+
+	////////////////////////////////////////////////////////////
+	/// \brief Default constructor
+	/// 
+	/// Constructs an empty combo box
+	/// 
+	////////////////////////////////////////////////////////////
+	ComboBox();
+
+	////////////////////////////////////////////////////////////
+	/// \brief Default constructor
+	/// 
+	/// \param position      Position set to button
+	/// \param size          Size of combo box area
+	/// \param buttonSize    Size of button
+	/// \param texture       Source texture
+	/// \param spriteDefault Sub-rectangle of the texture to assign to the default sprite
+	/// \param spriteActive  Sub-rectangle of the texture to assign to the active sprite
+	/// \param style         Style, used for the text
+	/// \param theme         Text theme assigned to the button (Can be empty)
+	/// \param strings       List of strings
+	/// \param items         Combo box items
+	/// \param index         Selected item index
+	/// \param scrollTime    Amount of time required to scroll through the combo box
+	/// \param onItemChanged Event raised when another item is selected
+	/// \param enabled       Enable this button
+	/// 
+	////////////////////////////////////////////////////////////
+	ComboBox(const sf::Vector2f& position, const sf::Vector2f& size, const sf::Vector2f& buttonSize, const sf::Texture& texture,
+	const sf::IntRect& spriteDefault, const sf::IntRect& spriteActive, const TextStyle* style, const TextTheme* theme,
+	const Strings* strings, const Items& items, size_t index, int scrollTime, Event1<ComboBox, size_t> onItemChanged, bool enabled = true);
+
+	////////////////////////////////////////////////////////////
+	/// \brief Selected item
+	/// 
+	////////////////////////////////////////////////////////////
+	sf::String getItem() const;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Index of the selected item
+	/// 
+	////////////////////////////////////////////////////////////
+	bool getIndex() const;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Sets items
+	///
+	/// \param items      List of items to assign to this combo box
+	/// \param newIndex   New index
+	/// \param raiseEvent Raise OnCheckedChanged event
+	/// 
+	////////////////////////////////////////////////////////////
+	void setItems(Items items, size_t newIndex, bool raiseEvent);
+
+	////////////////////////////////////////////////////////////
+	/// \brief Sets index
+	///
+	/// \param index      New index
+	/// \param raiseEvent Raise OnCheckedChanged event
+	/// 
+	////////////////////////////////////////////////////////////
+	void setIndex(size_t index, bool raiseEvent = true);
+
+	////////////////////////////////////////////////////////////
+	/// \brief Enables or disabled the combo box
+	///
+	/// \param enabled Enable
+	/// 
+	////////////////////////////////////////////////////////////
+	void setEnabled(bool enabled) override;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Updates the control within the application's main thread loop
+	///
+	////////////////////////////////////////////////////////////
+	void update() override;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Process sf::Event within the control
+	///
+	/// \param window Respective window
+	/// \param event  Event that was triggered
+	///
+	////////////////////////////////////////////////////////////
+	bool onWindowEvent(sf::RenderWindow& window, sf::Event event) override;
+
+	////////////////////////////////////////////////////////////
+	/// \brief Draw the control to a render target
+	///
+	/// \param target Render target to draw to
+	/// \param states Current render states
+	///
+	////////////////////////////////////////////////////////////
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+private:
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event raised when a child button is released
+	///
+	/// \param summand 1 to move right, -1 to move left
+	/// 
+	////////////////////////////////////////////////////////////
+	void onReleasedButton(uint8_t summand);
+
+	////////////////////////////////////////////////////////////
+	/// \brief Event raised when a child button is held
+	/// 
+	/// \param summand 1 to move right, -1 to move left
+	/// 
+	////////////////////////////////////////////////////////////
+	void onHoldButton(uint8_t summand);
+
+	////////////////////////////////////////////////////////////
+	/// \brief Moves index left or right
+	/// 
+	/// \param summand 1 to move right, -1 to move left
+	///
+	////////////////////////////////////////////////////////////
+	void moveIndex(int8_t summand);
+
+	////////////////////////////////////////////////////////////
+	// Member data
+	////////////////////////////////////////////////////////////
+	LocalizableTextComponent m_text;          //!< Text component
+	Event1<ComboBox, size_t> m_onItemChanged; //!< Event raised when another item is selected
+	Button                   m_buttonLeft;    //!< Leftmost button of the combo box
+	Button                   m_buttonRight;   //!< Rightmost button of the combo box
+	Items                    m_items;         //!< Combo box items
+	sf::String               m_item;          //!< Selected item of the combo box
+	size_t                   m_index;         //!< Index of the selected item
+	sf::Clock                m_clock;         //!< Internal clock for validating holding
+	bool                     m_restart;       //!< Determines if the clock should be restarted
+	int                      m_elapsed;       //!< Elapsed time of the clock;
+	int                      m_scrollTime;    //!< Amount of time required to scroll through the items
+	bool                     m_holdingButton; //!< Determines if a button was held
+};
+
+} //namespace le
+
+
+#endif // LEVEL_EDITOR_COMBOBOX_HPP
